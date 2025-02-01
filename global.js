60407ec0cd493d48c67cc5a7c0e63c99456c7f11
Zubin Sannakkayala <zubinsannakkayala@hotmail.com>
2025-01-31 16:41:01 -0800

    finish lab

--- global.js
+++ global.js
@@ -117,3 +117,8 @@ export function renderProjects(project, containerElement, headingLevel = 'h2') {
 
 }
 
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
+
+
---
+++ index.js
@@ -0,0 +1,17 @@
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
+const projects = await fetchJSON('./lib/projects.json');
+const latestProjects = projects.slice(0, 3);
+const projectsContainer = document.querySelector('.projects');
+renderProjects(latestProjects, projectsContainer, 'h2');
+const githubData = await fetchGitHubData('zubinss');
+const profileStats = document.querySelector('#profile-stats');
+if (profileStats) {
+    profileStats.innerHTML = `
+          <dl>
+            <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+            <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+            <dt>Followers:</dt><dd>${githubData.followers}</dd>
+            <dt>Following:</dt><dd>${githubData.following}</dd>
+          </dl>
+      `;
+  }
