9e75ab41692e409b11f9fdc2958ffc601d8ea049
Zubin Sannakkayala <zubinsannakkayala@hotmail.com>
2025-02-05 13:21:09 -0800

    i quit

--- projects/projects.js
+++ projects/projects.js
@@ -59,3 +59,91 @@ data.forEach((d, idx) => {
           .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
           .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
 })
+
+let query = '';
+
+function setQuery(newQuery) {
+    query = newQuery.toLowerCase(); 
+    let filteredProjects = projects.filter((project) => {
+        let values = Object.values(project).join('\n').toLowerCase();
+        return values.includes(query.toLowerCase());
+      });
+      return filteredProjects;
+  }
+  
+  let searchInput = document.getElementsByClassName('searchBar')[0];
+  
+  searchInput.addEventListener('change', (event) => {
+    let filteredProjects = setQuery(event.target.value);
+    renderProjects(filteredProjects, projectsContainer, 'h2');
+    // re-calculate rolled data
+    let newRolledData = d3.rollups(
+      filteredProjects,
+      (v) => v.length,
+      (d) => d.year,
+    );
+    // re-calculate data
+    let newData = newRolledData.map(([year, count]) => {
+      return {value: count, label: year }; // TODO
+    });
+    // re-calculate slice generator, arc data, arc, etc.
+    let newSliceGenerator = d3.pie().value((d) => d.value);
+    let newArcData = newSliceGenerator(newData);
+    let newArcs = newArcData.map((d) => arcGenerator(d));
+
+    // TODO: clear up paths and legends
+    d3.select('svg').selectAll('path').remove();
+    d3.select('.legend').selectAll('li').remove();
+
+    // update paths and legends, refer to steps 1.4 and 2.2
+    newArcs.forEach((arc, idx) => {
+        d3.select('svg')
+          .append('path')
+          .attr('d', arc)
+          .attr('fill', colors(idx));
+      });
+    let legend = d3.select('.legend');
+    newData.forEach((d, idx) => {
+        legend.append('li')
+        .attr('style', `--color:${colors(idx)}`)
+        .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
+    });
+  });
+
+
+
+//   5.2
+  let selectedIndex = -1;
+  for (let i = 0; i < arcs.length; i++) {
+    const svgNS = "http://www.w3.org/2000/svg"; // to create <path> tag in memory
+    let path = document.createElementNS(svgNS, "path");
+    
+    path.setAttribute("d", arcs[i]);
+    path.setAttribute("fill", colors(i));
+  
+    path.addEventListener('click', (event) => {
+      // What should we do?
+      if (selectedIndex === i) {
+        selectedIndex = -1; // Deselect
+      } else {
+        selectedIndex = i; // Select the clicked wedge
+      }
+    })
+  
+    let li = document.createElement('li');
+    li.style.setProperty('--color', colors(i));
+  
+    // Create the swatch span
+    let swatch = document.createElement('span');
+    swatch.className = 'swatch';
+    swatch.style.backgroundColor = colors(i);
+    
+    // Append the swatch to the list item
+    li.appendChild(swatch);
+  
+    // Set the label and value
+    li.innerHTML += `${data[i].label} <em>(${data[i].value})</em>`;
+  
+    legendNew.appendChild(li);
+    svg.appendChild(path);
+  }
