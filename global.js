2021dc7965a272422bd34adabfa798cd0bd5be68
Zubin Sannakkayala <zubinsannakkayala@hotmail.com>
2025-02-11 15:53:26 -0800

    step 0 lab 6

--- global.js
+++ global.js
@@ -41,6 +41,7 @@ let pages = [
   { url: 'projects/', title: 'Projects' },
   { url: 'contact/', title: 'Contact' },
   { url: 'resume/', title: 'Resume' },
+  { url: 'meta/', title: 'Meta' },
   { url: 'https://github.com/zubinss', title: 'GitHub' } ]
 
 
